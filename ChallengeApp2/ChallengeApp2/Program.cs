7be63f3a5614d3db3864b5c41b7caaf4c8b2ccac
Artur Borkowski <artur_borkowski@outlook.com>
2024-10-03 21:22:42 +0200

    D4, operacje relacyjne, logiczne, if-else, else-if, zagnieżdżony if

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,66 +1,68 @@
-﻿
-// var myAge = 33;
+﻿var number1 = 50;
+var number2 = 10;
 
-/*
-int myAge2 = 33;
-System.Console.WriteLine(myAge);
-System.Console.WriteLine(myAge2);
-*/
+// operatory relacyjne
+// == != > < <= >=
 
-// liczby całkowite
-int myAge = 120;
-int newAge = myAge + 5;
-Console.WriteLine(newAge);
-myAge = int.MaxValue;
-Console.WriteLine(myAge);
-int myVar = int.MinValue;
-Console.WriteLine(myVar);
-uint myVar2 = uint.MaxValue;
-Console.WriteLine(myVar2);
+if (number1 < number2)
+{
+    Console.WriteLine("JESTEM TUTAJ W LINI 6");
+}
+else
+{
+    Console.WriteLine("JESTEM W LINI 10");
+}
 
-long myVarLong = long.MaxValue;
-Console.WriteLine(myVarLong);
-myVarLong = long.MinValue;
-Console.WriteLine(myVarLong);
-ulong myVarULong = ulong.MaxValue;
-Console.WriteLine(myVarULong);
-myVarULong = ulong.MinValue;
-Console.WriteLine(myVarULong);
-Console.WriteLine();
 
-// liczby zmiennoprzecinkowe
-float myNumber = float.MaxValue;
-Console.WriteLine(myNumber);
-double myNumber2 = double.MaxValue;
-Console.WriteLine(myNumber2);
-decimal myNumber3 = decimal.MaxValue;
-Console.WriteLine(myNumber3);
-myNumber3 = decimal.MinValue;
-Console.WriteLine(myNumber3);
-Console.WriteLine();
+var name = "Adam";
+var age = 33;
 
-// zmienne tekstowe
-string name = "Adam";
-string surname = "Kamizelich";
-string result = name + surname;
-Console.WriteLine(result);
-string oneAsText = "1";
-int oneAsOne = 1;
-result = oneAsText + oneAsOne;
-Console.WriteLine(result);
-result = oneAsOne + oneAsText;
-Console.WriteLine(result);
-Console.WriteLine();
+// operatory logiczne
+// &&, ||, !
 
-// zmienne znakowe
-char letter = 'A';
-Console.WriteLine(letter);
-Console.WriteLine();
+if (name == "Adam" &&  age < 50)
+{
+    Console.WriteLine("JESTEM ADAMEM PRZED 50");
+} else
+{
+    Console.WriteLine("JESTEM KIMŚ INNYM");
+}
 
-// zmienne logiczne
-bool isActive = true;
-Console.WriteLine(isActive);
-isActive = false;
-Console.WriteLine(isActive);
-var isValid = 5 > 6;
-Console.WriteLine(isValid);
+if (name == "Adam" || age < 50)
+{
+    Console.WriteLine("JESTEM ADAMEM lub JESTEM PRZED 50");
+}
+else
+{
+    Console.WriteLine("JESTEM TUTAJ W LINI 37");
+}
+
+bool isTrue = true;
+Console.WriteLine(isTrue);
+isTrue = !isTrue;
+Console.WriteLine(isTrue);
+
+if (age > 50)
+{
+    if(name == "Adam")
+    {
+        Console.WriteLine("Jestem Adamem i mam więcej niż 50 lat");
+    }
+    else
+    {
+        Console.WriteLine("Więcej niż 50 lat");
+    }
+    
+}
+else if (age > 40)
+{
+    Console.WriteLine("Więcej niż 40 lat");
+}
+else if (age > 30)
+{
+    Console.WriteLine("Więcej niż 30 lat");
+}
+else
+{
+    Console.WriteLine("Więcej niż 20 lat");
+}

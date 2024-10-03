94fd9cf216d8456153a85b483e654fc51f60b737
Artur Borkowski <artur_borkowski@outlook.com>
2024-10-03 08:30:31 +0200

    D4, Typy danych

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// var myAge = 33;
+
+/*
+int myAge2 = 33;
+System.Console.WriteLine(myAge);
+System.Console.WriteLine(myAge2);
+*/
+
+// liczby całkowite
+int myAge = 120;
+int newAge = myAge + 5;
+Console.WriteLine(newAge);
+myAge = int.MaxValue;
+Console.WriteLine(myAge);
+int myVar = int.MinValue;
+Console.WriteLine(myVar);
+uint myVar2 = uint.MaxValue;
+Console.WriteLine(myVar2);
+
+long myVarLong = long.MaxValue;
+Console.WriteLine(myVarLong);
+myVarLong = long.MinValue;
+Console.WriteLine(myVarLong);
+ulong myVarULong = ulong.MaxValue;
+Console.WriteLine(myVarULong);
+myVarULong = ulong.MinValue;
+Console.WriteLine(myVarULong);
+Console.WriteLine();
+
+// liczby zmiennoprzecinkowe
+float myNumber = float.MaxValue;
+Console.WriteLine(myNumber);
+double myNumber2 = double.MaxValue;
+Console.WriteLine(myNumber2);
+decimal myNumber3 = decimal.MaxValue;
+Console.WriteLine(myNumber3);
+myNumber3 = decimal.MinValue;
+Console.WriteLine(myNumber3);
+Console.WriteLine();
+
+// zmienne tekstowe
+string name = "Adam";
+string surname = "Kamizelich";
+string result = name + surname;
+Console.WriteLine(result);
+string oneAsText = "1";
+int oneAsOne = 1;
+result = oneAsText + oneAsOne;
+Console.WriteLine(result);
+result = oneAsOne + oneAsText;
+Console.WriteLine(result);
+Console.WriteLine();
+
+// zmienne znakowe
+char letter = 'A';
+Console.WriteLine(letter);
+Console.WriteLine();
+
+// zmienne logiczne
+bool isActive = true;
+Console.WriteLine(isActive);
+isActive = false;
+Console.WriteLine(isActive);
+var isValid = 5 > 6;
+Console.WriteLine(isValid);

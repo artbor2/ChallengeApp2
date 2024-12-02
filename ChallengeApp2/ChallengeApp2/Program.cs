e95082da30b8182036bc662c9b4415e628aaeea5
Artur Borkowski <artbor2@gmail.com>
2024-12-02 22:03:01 +0100

    D5, List, Foreach

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,48 +1,20 @@
-﻿int[] grades = new int[3];
+﻿List<string> daysOfWeek = new List<string>();
+daysOfWeek.Add("Poniedziałek");
+daysOfWeek.Add("Wtorek");
+daysOfWeek.Add("Środa");
+daysOfWeek.Add("Czwartek");
+daysOfWeek.Add("Piatek");
+daysOfWeek.Add("Sobota");
+daysOfWeek.Add("Niedziela");
 
-string[] daysOfWeek = new string[7];
-daysOfWeek[0] = "Poniedziałek";
-daysOfWeek[1] = "Wtorek";
-daysOfWeek[2] = "Środa";
-daysOfWeek[3] = "Czwartek";
-daysOfWeek[4] = "Piatek";
-daysOfWeek[5] = "Sobota";
-daysOfWeek[6] = "Niedziela";
-Console.WriteLine(daysOfWeek[2]);
-
-string[] daysOfWeek2 = {
-    "Poniedziałek", "Wtorek", "Środa",
-    "Czwartek", "Piątek","Sobota",
-    "Niedziela"
-};
-Console.WriteLine(daysOfWeek2[0]);
-Console.WriteLine();
-
-/*
- * Index was outside the bounds of the array 
- * 
- * Console.WriteLine(daysOfWeek2[-1]);
- * Console.WriteLine(daysOfWeek2[10]);
- */
-
-
-for (int i = 0; i < 10; i = i + 1)
-{
-    Console.Write(i + " ");
-}
-Console.WriteLine();
-
-
-for (int i = 0; i < 10; i++)
-{
-    Console.Write(i + " ");
-}
-Console.WriteLine('\n');
-
-
-
-for(int i = 0; i < daysOfWeek.Length; i++)
+for(int i = 0; i < daysOfWeek.Count; i++)
 {
     Console.WriteLine(daysOfWeek[i]);
+}
+Console.WriteLine();
+
+foreach (var day in daysOfWeek)
+{
+    Console.WriteLine(day);
 }
 

b6187e79a6ca12a6301733052bd2dcccba5abf07
Artur Borkowski <artur_borkowski@outlook.com>
2024-12-26 08:22:37 +0100

    D6, Konstruktory

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,13 +1,39 @@
 ﻿User user1 = new User();
-User user2 = new User();
-User user3 = new User();
-User user4 = new User();
+User user2 = new User("Monika");
+User user3 = new User("Zuzia", "47367367");
+User user4 = new User("Damian", "duddfcfs", "Damian");
 
-user1.login = "Adam";
-user1.password = "password";
 class User
 {
     // default private
-    public string login;
-    public string password;    
+    private string login;
+    private string password;
+    private string name;
+
+    public User()
+    {
+        this.login = "_";
+        this.password = "_";
+        this.name = "_";
+
+    }
+
+    public User(string login)
+    {
+        this.login = login;
+        this.password = "_";
+        this.name = "_";
+    }
+
+    public User(string login, string password)
+    {
+        this.login = login;
+        this.password = password;
+        this.name = "_";
+    }
+
+    public User(string login, string password, string name) : this(login, password)
+    {
+        this.name = name;
+    }
 }

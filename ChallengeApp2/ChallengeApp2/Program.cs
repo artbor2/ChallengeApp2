5fb498ac5d364b6aa4e20c01dfe6421bb8fc8c82
Artur Borkowski <artur_borkowski@outlook.com>
2024-12-31 08:01:32 +0100

    D6, pola, metody, hermetyzacja, statyczne pola i metody

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,39 +1,47 @@
-﻿User user1 = new User();
-User user2 = new User("Monika");
+﻿User user1 = new User("Adam", "356647477");
+User user2 = new User("Monika", "465437848");
 User user3 = new User("Zuzia", "47367367");
-User user4 = new User("Damian", "duddfcfs", "Damian");
+User user4 = new User("Damian", "duddfcfs");
+
+string login = user1.Login;
+string password = user1.Password;
+user1.AddScore(5);
+user1.AddScore(2);
+user1.AddScore(1);
+
+int result = user1.Result;
+Console.WriteLine(result);
+Console.WriteLine(User.GameName);
+var pi = Math.PI;
+Console.WriteLine(pi);
+Console.WriteLine(User.count); 
 
 class User
 {
+    public static string GameName = "Diablo";
+    public static int count = 0;
+    private List<int> scores = new List<int>();
+
     // default private
-    private string login;
-    private string password;
-    private string name;
-
-    public User()
-    {
-        this.login = "_";
-        this.password = "_";
-        this.name = "_";
-
-    }
-
-    public User(string login)
-    {
-        this.login = login;
-        this.password = "_";
-        this.name = "_";
-    }
-
     public User(string login, string password)
     {
-        this.login = login;
-        this.password = password;
-        this.name = "_";
+        this.Login = login; 
+        this.Password = password;
+        User.count++;
     }
 
-    public User(string login, string password, string name) : this(login, password)
+    public string Login { get; private set; }
+    public string Password { get; private set; }
+    public int Result
     {
-        this.name = name;
+        get
+        {
+            return this.scores.Sum();
+        }
+    }
+
+    public void AddScore(int score)
+    {
+        scores.Add(score);
     }
 }
